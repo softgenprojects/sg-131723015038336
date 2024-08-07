793d8c2cef9eb46d724eab34a5bafe6cf9629447
SoftGen <admin@softgen.ai>
2024-08-07 07:36:49 +0000

    SOFTGEN MAGIC

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </ThemeProvider>
   );
 }
-
